1a4c06a32d91acb77a15856c7f813bf63c4aa7c2
giuseppebrandi-byui <bra22041@byui.edu>
2025-09-25 10:18:33 +0100

    Cleaned register.razor page

--- obj/Debug/net9.0/cse325-Team6-Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/cse325-Team6-Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cse325-Team6-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9669394f434f6d98ed48065c1819c2ea7e2f55d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdd9bce2809a97a1b2096fc096ec3d61acfa7cef")]
 [assembly: System.Reflection.AssemblyProductAttribute("cse325-Team6-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cse325-Team6-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
