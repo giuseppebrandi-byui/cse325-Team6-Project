1aae9bc2526f91e347800c54e6b59c26b9bdbece
Antonio Melendez <mel23001@byui.edu>
2025-09-30 14:04:00 -0600

    services

--- obj/Debug/net9.0/cse325-Team6-Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/cse325-Team6-Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cse325-Team6-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7ee2d98324bb3fe88a4a6498241e65f6d755685")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b80242b9abf9f3443b8da015d70ceb0ed1d4d7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("cse325-Team6-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cse325-Team6-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
