aaf35d3d053dd82fa214420fbae716c0326a51b1
Antonio Melendez <mel23001@byui.edu>
2025-10-11 19:19:36 -0600

    update

--- obj/Debug/net9.0/cse325-Team6-Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/cse325-Team6-Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cse325-Team6-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aae9bc2526f91e347800c54e6b59c26b9bdbece")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ff48a9e6f8a76c4f0dd33624e2570276748393a")]
 [assembly: System.Reflection.AssemblyProductAttribute("cse325-Team6-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cse325-Team6-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
